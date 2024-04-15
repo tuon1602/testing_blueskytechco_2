6468bbdb013246a8cc5ebd74e871b7be004e05b0
tuon1602 <tuan69878@gmail.com>
2024-04-15 16:31:54 +0700

    fix

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-console.log("Hello")
+// console.log("Hello")
 
 
-const myH1 = document.getElementById("myh1").innerHTML="Hello"
+// const myH1 = document.getElementById("myh1").innerHTML="Hello"
 
-const myButton = document.getElementById("myButton")
+// const myButton = document.getElementById("myButton")
 
-myButton.addEventListener("click",(event)=>{
-    console.log('Event type:', event.type);
-    console.log('Target element:', event.target);
-    console.log('Current target element:', event.currentTarget);
-    console.log('Time stamp:', event.timeStamp);
+// myButton.addEventListener("click",(event)=>{
+//     console.log('Event type:', event.type);
+//     console.log('Target element:', event.target);
+//     console.log('Current target element:', event.currentTarget);
+//     console.log('Time stamp:', event.timeStamp);
     
-    // Preventing the default action
-    event.preventDefault();
-})
+//     // Preventing the default action
+//     event.preventDefault();
+// })
