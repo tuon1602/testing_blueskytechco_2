e86922d95c352284e854ad48780b78e1ed6c1e12
tuon1602 <tuan69878@gmail.com>
2024-04-15 16:20:32 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello")
+
+
+const myH1 = document.getElementById("myh1").innerHTML="Hello"
+
+const myButton = document.getElementById("myButton")
+
+myButton.addEventListener("click",(event)=>{
+    console.log('Event type:', event.type);
+    console.log('Target element:', event.target);
+    console.log('Current target element:', event.currentTarget);
+    console.log('Time stamp:', event.timeStamp);
+    
+    // Preventing the default action
+    event.preventDefault();
+})
